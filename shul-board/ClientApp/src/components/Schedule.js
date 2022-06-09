b13ecef1fd7bb01f9461bccc1e568fd1b16e5a0e
Shmuly Engelman <shmulyeng@gmail.com>
2022-06-09 13:42:21 -0400

    scrolling schedule

--- shul-board/ClientApp/src/components/Schedule.js
+++ shul-board/ClientApp/src/components/Schedule.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import TimeItem from './TimeItem';
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Schedule({ schedule, loading }) {
     const renderSchedule = (schedule) => {
         console.log(schedule);
+        const settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            adaptiveHeight: true,
+            arrows: false,
+            pauseOnHover: false,
+            initialSlide: 0,
+            className: "slide",
+            vertical: false
+        };
         return (
             <div className='scheduleContainer panel schedulePanel'>
-                {schedule.map(group =>
-                    <div key={group.id} className='scheduleitem'>
-                        <div className='scheduleGroupName'>{group.name}</div>
-                        {group.items.map(item =>
-                            <TimeItem key={item.id} name={item.name} time={item.time} description={item.description} />
-                        )}
-                    </div>
-                )}
-
+                <Slider {...settings}>
+                    {schedule.map(group =>
+                        <div key={group.id} className='scheduleitem'>
+                            <div className='scheduleGroupName'>{group.name}</div>
+                            {group.items.map(item =>
+                                <TimeItem key={item.id} name={item.name} time={item.time} description={item.description} />
+                            )}
+                        </div>
+                    )}
+                </Slider>
             </div>
         );
     }
